f59438204c2469ba4be4e9d6f4d13236fafd52e8
bingbang <44920040@qq.com>
2020-02-14 20:14:17 +0800

    Initial commit

---
+++ src/main/java/com/github/startzyp/minecraft/DeathDropLore.java
@@ -0,0 +1,51 @@
+package com.github.startzyp.minecraft;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class DeathDropLore extends JavaPlugin implements Listener {
+    @EventHandler
+    public void PlayerDeatheent(PlayerDeathEvent event){
+        Player entity = event.getEntity();
+        PlayerInventory inventory = entity.getInventory();
+        for (int a=0;a<=35;a++){
+            ItemStack item = inventory.getItem(a);
+            if (item==null||item.getType()== Material.AIR){
+                continue;
+            }
+            if (!item.hasItemMeta()){
+                continue;
+            }
+            if (!item.getItemMeta().hasLore()){
+                continue;
+            }
+            String s = item.getItemMeta().getLore().toString();
+            System.out.println(s);
+            if (s.contains(getConfig().getString("Lore"))){
+                inventory.setItem(a,null);
+                entity.getWorld().dropItemNaturally(entity.getLocation().add(0,2,0),item);
+            }
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        File config = new File(getDataFolder() + File.separator + "config.yml");
+        if (!config.exists()) {
+            getConfig().options().copyDefaults(true);
+        }
+        saveDefaultConfig();
+        Bukkit.getServer().getPluginManager().registerEvents(this,this);
+        super.onEnable();
+    }
+}
